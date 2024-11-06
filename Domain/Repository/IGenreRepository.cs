f2fd17140edde05116c6a7743ae16d56940c8482
Danny <thanhan1100@gmail.com>
2024-11-06 16:16:42 +0700

    Update on GenreRepository Interfaces

--- Domain/Repository/IGenreRepository.cs
+++ Domain/Repository/IGenreRepository.cs
@@ -1,5 +1,22 @@
-﻿namespace Domain.Repository;
+﻿// Domain
+using Domain.Entities;
 
-public interface IGenreRepository
+namespace Domain.Repository
 {
+    public interface IGenreRepository
+    {
+        // == Basic CRUD Function ==
+        public Task<IEnumerable<Genre>> GetAll();
+        public Task Add(Genre genre);
+        public Task Update(Genre genre);
+        public Task Delete(int id);
+
+        // == Feature Function ==
+
+        // Search by Genre ID
+        public Task<Genre> GetById(int id);
+
+        // TODO: Search By Name
+
+    }
 }
