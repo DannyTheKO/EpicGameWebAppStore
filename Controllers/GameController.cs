7c9ce357465ba0d7bca0b0f0c2566f53afe8c01a
Danny <thanhan1100@gmail.com>
2024-11-06 15:53:27 +0700

    Add CRUD operations to GameController
    Updated GameController to include IGenreService and IPublisherService.
    Modified constructor to initialize new services. Reformatted Index method.
    Enhanced Create method with GET and POST versions for form handling.
    Added Update methods for retrieving and updating games.
    Implemented Delete methods for game deletion.
    Removed commented-out TODO sections and replaced with actual implementations.

--- Controllers/GameController.cs
+++ Controllers/GameController.cs
@@ -1,8 +1,10 @@
 ﻿using System.Security.Claims;
 using Application.Interfaces;
 using Application.Services;
+using Domain.Entities;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace EpicGameWebAppStore.Controllers;
 
@@ -10,45 +12,115 @@ namespace EpicGameWebAppStore.Controllers;
 public class GameController : _BaseController
 {
 	private readonly IGameService _gameServices;
+    private readonly IGenreService _genreService; 
+    private readonly IPublisherService _publisherService; 
+
     private readonly IAuthenticationServices _authenticationServices;
 	private readonly IAuthorizationServices _authorizationServices;
 
-	public GameController(IGameService gameServices, IAuthenticationServices authenticationServices, IAuthorizationServices authorizationServices)
+	public GameController(
+        IGameService gameServices,
+        IGenreService genreService, 
+        IPublisherService publisherService,
+        IAuthenticationServices authenticationServices, 
+        IAuthorizationServices authorizationServices)
 		: base(authenticationServices, authorizationServices)
 	{
 		_gameServices = gameServices;
+        _genreService = genreService;
+        _publisherService = publisherService;
         _authenticationServices = authenticationServices;
 		_authorizationServices = authorizationServices;
 	}
 
 	// GET: Game/Index
-	public async Task<IActionResult> Index()
-	{
-		var games = await _gameServices.GetAllGameAsync();
-		return View(games);
-	}
+        // GET: Game/Index
+        public async Task<IActionResult> Index()
+        {
+            var games = await _gameServices.GetAllGameAsync();
+            return View(games);
+        }
 
-	// TODO: Create Function
-	// GET: Game/Create
-	public async Task<IActionResult> Create()
-	{
-		bool hasPermission = await _authorizationServices.UserHasPermission(GetCurrentLoginAccountId(), "delete");
+        // GET: Game/Create
+        public async Task<IActionResult> Create()
+        {
+            // Chờ kết quả từ các phương thức bất đồng bộ
+            ViewBag.GenreId = new SelectList(await _genreService.GetAllGenresAsync(), "GenreId", "Name");
+            ViewBag.PublisherId = new SelectList(await _publisherService.GetAllPublishersAsync(), "PublisherId", "Name");
 
-		if (!hasPermission) // Dont have permission of that role
-		{
-			return RedirectToAction("AccessDenied", "Auth");
-		}
+            return View();
+        }
 
-		return View();
-	}
+        // POST: Game/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(Game game)
+        {
+            if (ModelState.IsValid)
+            {
+                await _gameServices.AddGameAsync(game); // Assuming you have an AddGameAsync method
+                return RedirectToAction(nameof(Index));
+            }
 
-	// POST: Game/Create
+            ViewBag.GenreId = new SelectList(await _genreService.GetAllGenresAsync(), "GenreId", "Name", game.GenreId);
+            ViewBag.PublisherId = new SelectList(await _publisherService.GetAllPublishersAsync(), "PublisherId", "Name", game.PublisherId);
+            return View(game);
+        }
 
-	// TODO: Update Function 
-	// GET: Game/Update/{id}
-	// POST: Game/Update/{id}
+        // GET: Game/Update/{id}
+        public async Task<IActionResult> Update(int id)
+        {
+            var game = await _gameServices.GetGameByIdAsync(id); // Assuming this method exists
+            if (game == null)
+            {
+                return NotFound();
+            }
 
-	// TODO: Delete Function
-	// GET: Game/Delete/{id}
-	// POST: Game/Delete/{id}
+            ViewBag.GenreId = new SelectList(await _genreService.GetAllGenresAsync(), "GenreId", "Name", game.GenreId);
+            ViewBag.PublisherId = new SelectList(await _publisherService.GetAllPublishersAsync(), "PublisherId", "Name", game.PublisherId);
+            return View(game);
+        }
+
+        // POST: Game/Update/{id}
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(int id, Game game)
+        {
+            if (id != game.GameId)
+            {
+                return BadRequest();
+            }
+
+            if (ModelState.IsValid)
+            {
+                await _gameServices.UpdateGameAsync(game); // Assuming you have an UpdateGameAsync method
+                return RedirectToAction(nameof(Index));
+            }
+
+            ViewBag.GenreId = new SelectList(await _genreService.GetAllGenresAsync(), "GenreId", "Name", game.GenreId);
+            ViewBag.PublisherId = new SelectList(await _publisherService.GetAllPublishersAsync(), "PublisherId", "Name", game.PublisherId);
+            return View(game);
+        }
+
+        // GET: Game/Delete/{id}
+        public async Task<IActionResult> Delete(int id)
+        {
+            var game = await _gameServices.GetGameByIdAsync(id); // Assuming this method exists
+            if (game == null)
+            {
+                return NotFound();
+            }
+
+            return View(game);
+        }
+
+        // POST: Game/Delete/{id}
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            await _gameServices.DeleteGameAsync(id); // Assuming you have a DeleteGameAsync method
+            return RedirectToAction(nameof(Index));
+        }
+
 }
