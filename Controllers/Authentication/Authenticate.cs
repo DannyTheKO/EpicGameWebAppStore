febc4a7ca88a0a5c3deded4ddd00a58ecccaf480
Danny <thanhan1100@gmail.com>
2024-12-06 00:24:46 +0700

    Refactor authentication services and controllers
    Updated IAuthenticationServices interface to include ForgotPassword method and modified GenerateJwtToken method. Refactored AuthenticationServices class to reintroduce constructor and methods, added ForgotPassword method, and updated GenerateJwtToken method. Created Authenticate controller to consolidate authentication actions, removed Login and Register controllers. Added ForgotPasswordForm model for forgot password functionality.

---
+++ Controllers/Authentication/Authenticate.cs
@@ -0,0 +1,137 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+using EpicGameWebAppStore.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace EpicGameWebAppStore.Controllers.Authentication;
+
+[Route("[controller]")]
+[ApiController]
+public class Authenticate : _BaseController
+{
+	private readonly IAuthorizationServices _authorizationServices;
+	private readonly IAuthenticationServices _authenticationServices;
+	private readonly IAccountService _accountService;
+
+	public Authenticate(
+		IAuthorizationServices authorizationServices,
+		IAuthenticationServices authenticationServices,
+		IAccountService accountService)
+		: base(authorizationServices)
+	{
+		_authorizationServices = authorizationServices;
+		_authenticationServices = authenticationServices;
+		_accountService = accountService;
+	}
+
+	[HttpGet("Register")]
+	public async Task<ActionResult> RegisterTask(RegisterViewModel request)
+	{
+		// Check if the form is valid
+		if (!ModelState.IsValid) // Requirement is not satisfied => FAIL
+		{
+			return BadRequest(new
+			{
+				registerState = false,
+				errors = ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(e => e.ErrorMessage)
+			});
+		}
+		var account = new Account()
+		{
+			Username = request.Username,
+			Password = request.Password
+		};
+		var (state, message) = await _authenticationServices.RegisterAccount(account, request.ConfirmPassword);
+		if (!state)
+		{
+			return Unauthorized(new
+			{
+				registerState = state,
+				registerMessage = message,
+			});
+		}
+		return Ok(new
+		{
+			registerState = state,
+			registerMessage = message,
+		});
+	}
+
+	[HttpGet("Login")]
+	public async Task<ActionResult> LoginTask(LoginViewModel request)
+	{
+		// Check if the form is valid
+		if (!ModelState.IsValid) // Requirement is not satisfied => FAIL
+		{
+			return BadRequest(new
+			{
+				loginState = false,
+				errors = ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(e => e.ErrorMessage)
+			});
+		}
+
+		var account = new Account()
+		{
+			Username = request.Username,
+			Password = request.Password
+		};
+
+		var (state, token, message) = await _authenticationServices.LoginAccount(account);
+
+		if (!state)
+		{
+			return Unauthorized(new
+			{
+				loginState = state,
+				loginMessage = message,
+			});
+		}
+
+		return Ok(new
+		{
+			loginState = state,
+			loginMessage = message,
+			loginToken = token
+		});
+	}
+
+	[HttpGet("ForgotPassword")]
+	public async Task<ActionResult> ForgotPasswordTask(ForgotPasswordForm request)
+	{
+		// Check if the form is valid
+		if (!ModelState.IsValid)
+		{
+			return BadRequest(new
+			{
+				forgotPasswordState = false,
+				errors = ModelState.Values
+					.SelectMany(v => v.Errors)
+					.Select(e => e.ErrorMessage)
+			});
+		}
+
+		var (state, message) = await _authenticationServices.ForgotPassword(request.Username, request.Email);
+
+		if (!state)
+		{
+			return BadRequest(new
+			{
+				forgotPasswordState = state,
+				message = message
+			});
+		}
+
+		return Ok(new
+		{
+			forgotPasswordState = state,
+			message = message
+		});
+	}
+
+}
